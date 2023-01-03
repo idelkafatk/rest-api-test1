9b6c0e334c04fe27318b8960101a94f1bb405b55
idelfatkhullin <maximbravin@gmail.com>
2023-01-03 20:50:44 +0600

    added api tests for regres.in

---
+++ src/test/java/RestAssuredTests.java
@@ -0,0 +1,84 @@
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static io.restassured.http.ContentType.JSON;
+import static org.hamcrest.Matchers.is;
+
+public class RestAssuredTests {
+    @Test
+    void successAuthTest() {
+        String data = "{ \"email\": \"eve.holt@reqres.in\", \"password\": \"cityslicka\" }";
+
+        given()
+                .log().uri()
+                .contentType(JSON)
+                .body(data)
+                .when()
+                .post("https://reqres.in/api/login")
+                .then()
+                .log().status()
+                .log().body()
+                .statusCode(200)
+                .body("token", is("QpwL5tke4Pnpja7X4"));
+    }
+
+    @Test
+    void unSuccessAuthTest() {
+        String data = "{ \"email\": \"eve.holt@reqres.in\" }";
+
+        given()
+                .log().uri()
+                .contentType(JSON)
+                .body(data)
+                .when()
+                .post("https://reqres.in/api/login")
+                .then()
+                .log().status()
+                .log().body()
+                .statusCode(400)
+                .body("error", is("Missing password"));
+    }
+
+    @Test
+    void userNotFoundTest() {
+
+        given()
+                .log().uri()
+                .when()
+                .get("https://reqres.in//api/users/23")
+                .then()
+                .log().status()
+                .statusCode(404);
+    }
+
+    @Test
+    void getListOfUsersTest() {
+
+        given()
+                .log().uri()
+                .when()
+                .get("https://reqres.in/api/users?page=2")
+                .then()
+                .log().status()
+                .statusCode(200);
+    }
+
+    @Test
+    void successRegisterTest() {
+        String data = "{ \"email\": \"eve.holt@reqres.in\", \"password\": \"pistol\" }";
+
+        given()
+                .log().uri()
+                .contentType(JSON)
+                .body(data)
+                .when()
+                .post("https://reqres.in/api/register")
+                .then()
+                .log().status()
+                .log().body()
+                .statusCode(200)
+                .body("id", is(4))
+                .body("token", is("QpwL5tke4Pnpja7X4"));
+    }
+
+}
